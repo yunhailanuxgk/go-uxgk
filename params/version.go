95a969f8bcfd5082bc4b4e5e27237563ab950d7c
cc14514 <lance_fox0451@163.com>
2019-12-05 23:51:00 +0800

    reset vsn 0.2.0

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 0 // Major version component of the current release
-	VersionMinor = 1 // Minor version component of the current release
+	VersionMinor = 2 // Minor version component of the current release
 	VersionPatch = 0 // Patch version component of the current release
-	VersionMeta  = "rc"
+	VersionMeta  = "dev"
 )
 
 // Version holds the textual version string.
